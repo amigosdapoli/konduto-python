31c2e4c9bb83d3373ad0c551f0e273893df0e404
Fernando Paiva <fernandosjp@gmail.com>
2018-03-06 20:23:51 -0300

    First test to test API connection

--- tests.py
+++ tests.py
@@ -3,6 +3,7 @@
 import unittest
 import os
 from konduto import Konduto
+from konduto.models import Order
 from random import randint
 # Load .env variables
 from dotenv import load_dotenv, find_dotenv
@@ -18,9 +19,16 @@ class KondutoTestCase(unittest.TestCase):
     def setUp(self):
         self.konduto = Konduto(KONDUTO_PUBLIC_KEY, KONDUTO_PRIVATE_KEY)
 
-    def test_check_order(self):
-        response = self.konduto.send_order()
-        self.assertEqual(response["status"],"ok")
+    def _build_simple_order(self, info={}):
+        unique_id = randint(1, 100000)
+        order = Order()
+        return order
+
+    def test_expect_recommendation_approve(self):
+        order = self._build_simple_order()
+        response = self.konduto.analyze(order)
+
+        self.assertEqual(response['order']['recommendation'],'APPROVE')
 
 
 if __name__ == '__main__':
